2c7752239ca40b20ee03a2d504542d5ce2f0dd6d
Silvio Senese <s228789@studenti.polito.it>
2019-03-27 12:24:49 +0100

    Interfaccia grafica fatta

--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/controller/SegreteriaStudentiController.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/controller/SegreteriaStudentiController.java
@@ -1,5 +1,80 @@
 package it.polito.tdp.lab04.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class SegreteriaStudentiController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> comboBoxCorsi;
+
+    @FXML
+    private Button btnCercaIscritti;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private Button btnCompleta;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCercaCorsi;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCompleta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrizione(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert comboBoxCorsi != null : "fx:id=\"comboBoxCorsi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCercaIscritti != null : "fx:id=\"btnCercaIscritti\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCompleta != null : "fx:id=\"btnCompleta\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+
+    }
 }
--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/model/Studente.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/model/Studente.java
@@ -2,4 +2,13 @@ package it.polito.tdp.lab04.model;
 
 public class Studente {
 	
+	private int matricola; //chiave primaria
+	private String cognome;
+	private String nome;
+	private String cds;
+	
+	
+	
+	
+	
 }
